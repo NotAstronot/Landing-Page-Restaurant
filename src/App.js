227e5f2fa683a7ed550cdf3fe161963475dd2bec
NotAstronot <feririski409@gmail.com>
2023-07-04 14:51:10 +0700

    final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from "./components/Home";
+import About from "./components/About";
+import Testimonial from "./components/Testimonial";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Work from "./components/Work";
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+        <Home />
+        <About />
+        <Work />
+        <Testimonial />
+        <Contact />
+        <Footer />
     </div>
   );
 }
